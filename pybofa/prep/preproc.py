0332d00240c923c7658ad68f4077d94164ea816a
jag <jag5m33@gmail.com>
2026-03-16 21:44:59 +0000

    repo organisation

---
+++ pybofa/prep/preproc.py
@@ -0,0 +1,7 @@
+#log transform biomarker scores if skewed 
+#standardise features using Z score 
+# one hot encode sex as binary (0/1)
+
+#biomarker ranges MUST be scaled
+# age levels MUST be scale 0-100
+
--- pybofa_prep/preproc.py
+++
@@ -1,3 +0,0 @@
-#log transform biomarker scores if skewed 
-#standardise features using Z score 
-# encode sex as binary (0/1)
